84e3bf3413723d5ed0bfd0934624f3bcd3e9e995
Charlie <C2239060@live.tees.ac.uk>
2026-07-16 20:17:41 +0100

    Removed local network API URL

--- Todo List App/Program.cs
+++ Todo List App/Program.cs
@@ -9,8 +9,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173",
-                                "http://192.168.1.151:5173")
+            policy.WithOrigins("http://localhost:5173")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
